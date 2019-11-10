85c76c9296779b9aec0643787e7d4ed496549532
PromasterGuru <pmutondo12@gmail.com>
2019-11-10 11:02:02 +0300

    Implement the MainActivity screen

--- app/src/main/java/com/example/familygallery/view/MainActivity.kt
+++ app/src/main/java/com/example/familygallery/view/MainActivity.kt
@@ -8,27 +8,27 @@ import com.example.familygallery.R
 import com.example.familygallery.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var mainBinding : ActivityMainBinding
+    lateinit var mainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val mainBinding: ActivityMainBinding = DataBindingUtil.setContentView(
-            this, R.layout.activity_main) as ActivityMainBinding
+        val mainBinding: ActivityMainBinding =
+            DataBindingUtil.setContentView(
+                this, R.layout.activity_main
+            ) as ActivityMainBinding
 
         //Set actions to perform upon click
-        mainBinding.addMembers.setOnClickListener{
+        mainBinding.addMembers.setOnClickListener {
             val intent = Intent(this, AddMembersActivity::class.java)
             startActivity(intent)
-            
+
         }
-        mainBinding.viewMembers.setOnClickListener{
+        mainBinding.viewMembers.setOnClickListener {
             val intent = Intent(this, ViewMembersActivity::class.java)
             startActivity(intent)
         }
-        mainBinding.editMembers.setOnClickListener{
+        mainBinding.editMembers.setOnClickListener {
             val intent = Intent(this, EditMembersActivity::class.java)
             startActivity(intent)
         }
     }
-
-
 }
