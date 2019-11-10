b4b6d147e63cba5803ea55df778185c83e6ba41d
PromasterGuru <pmutondo12@gmail.com>
2019-11-10 14:45:25 +0300

    Revert "Revert "Ft add members activity""

---
+++ app/src/main/java/com/example/familygallery/view/ViewMembersActivity.kt
@@ -0,0 +1,13 @@
+package com.example.familygallery.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.familygallery.R
+
+class ViewMembersActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_members)
+    }
+}
